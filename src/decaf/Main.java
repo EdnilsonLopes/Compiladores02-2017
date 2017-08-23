93ab89cb34f3be2eec38f61339d9c5e4fc7e2737
aluno <leonardo.vasconcelos@hotmail.com.br>
2017-08-23 11:11:19 -0300

    Import inicial do repositório

--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -34,7 +34,14 @@ class Main {
 		        			case DecafLexer.ID:
 		        				type = " IDENTIFIER";
 		        				break;
+						case DecafLexer.CHAR:
+		        				type = " CHARLITERAL";
+		        				break;
+						case DecafLexer.STRING:
+		        				type = " STRING";
+		        				break;
 		        			}
+	
 		        			System.out.println (token.getLine() + type + " " + text);
 		        		}
 		        		done = true;
